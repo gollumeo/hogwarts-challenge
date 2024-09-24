15aee70a9c281c80a29e4d98b990cc333a13f695
Golluméo <p.mauriello54@gmail.com>
2024-09-24 17:09:43 +0200

    chore: pick the decision to swap from nest to adonisjs instead, because of its ressemblance with Laravel, thus making it easier for me to learn node (and TS)

--- backend/src/shared/application/interfaces/repository.interface.ts
+++
@@ -1,3 +0,0 @@
-export interface RepositoryInterface<T> {
-  findAll(): Promise<T[]>;
-}
--- backend/src/shared/application/interfaces/repositoryFactory.interface.ts
+++
@@ -1,9 +0,0 @@
-import { RepositoryInterface } from './repository.interface';
-
-export interface RepositoryFactoryInterface {
-  createRepository<T>(domainName: string): RepositoryInterface<T>;
-  registerDomain<T>(
-    domainName: string,
-    repositoryConstructor: new () => RepositoryInterface<T>,
-  ): void;
-}
